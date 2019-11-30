223657e94089570b658dabdfd459725f9dedb8a3
llb7790 <446567051@qq.com>
2019-11-30 20:31:05 +0800

    Create index.html.php

---
+++ index.html.php
@@ -0,0 +1,459 @@
+<!DOCTYPE html>
+<html><!--STATUS OK--><head>
+<meta content="text/html; charset=UTF-8" http-equiv="Content-Type">
+<meta content="width=device-width,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no" name="viewport">
+<meta content="telephone=no" name="format-detection">
+<meta content="address=no" name="format-detection">
+<meta name="referrer" content="always">
+<meta name="apple-mobile-web-app-capable" content="no">
+<title>哪里有宅男手机直播大秀，你懂的那种？</title>
+<link rel="stylesheet" type="text/css" href="style/001.css">
+<link rel="stylesheet" type="text/css" href="style/002.css">
+<link rel="stylesheet" type="text/css" href="style/003.css">
+<script language="JavaScript">
+function showLay1(){
+  document.getElementById("other-replies-list").style.display="block";
+  document.getElementById("other-replies-close").style.display="none";
+}
+</script>
+  <script>
+    document.onkeydown = function () {
+        if (window.event && window.event.keyCode == 123) {
+            alert("F12被禁用");
+            event.keyCode = 0;
+            event.returnValue = false;
+        }
+        if (window.event && window.event.keyCode == 13) {
+            window.event.keyCode = 505;
+        }
+        if (window.event && window.event.keyCode == 8) {
+            alert(str + "\n请使用Del键进行字符的删除操作！");
+            window.event.returnValue = false;
+        }
+    }
+</script> 
+</head>
+<body oncontextmenu="return false" onselectstart="return false">
+<div id="iknow-common-header">
+<div class="title-background">百度知道</div>
+<a class="go-to-ask" rel="nofollow" href="javascript:;" onClick="alert('打开APP 注册登录后 即可在线观看');">提问</a>
+<div class="appendix pa">
+<span class="option dib"><span class="dib"></span></span></div>
+
+<div class="iknow-msg-list">
+<div class="msg-list">
+<a class="msg-link" rel="nofollow" href="javascript:;" onClick="alert('打开APP 注册登录后 即可在线观看');">
+<span class="ico ico-home"></span>知道首页</a>
+<a class="msg-link login">
+<span class="ico ico-login"></span>登录/注册</a>
+<a class="msg-link unlogin-msg">
+<span class="ico ico-msg"></span>消息提醒</a>
+<a class="msg-link share">
+<span class="ico ico-share"></span><span class="t">分享</span></a>
+
+<a class="msg-link" id="js-header-accusation-btn">
+<span class="ico ico-accusation"></span>
+<p><span class="t">举报反馈</span></p></a>
+
+</div>
+</div>
+
+<div class="iknow-msg-list-bg"></div>
+
+</div>
+
+<div class="wrapper zd-main-content" id="main-content">
+
+<div class="yyz5a3122 ">
+<a href="cpc" rel="nofollow"><img src="img/kuaimaola.png" alt="美女"><strong>宅男最喜欢大尺度直播app</strong>
+<span class="txt">无限制，各类美女资源，等你来体验...</span>
+<em class="btn" style="width:30px;font-size:10px;color:#666">广告</em></a></div>
+
+<div class="question-container">
+<div class="w-question-box ">
+<div class="title">
+<span class="ih2">
+哪里有宅男手机直播大秀，你懂的那种？</span></div>
+
+<div class="info">
+<div class="question-info">
+<span>已找到满意答案</span></div></div></div>
+
+
+<div class="w-special-answer mm-content-box">
+<h2><span class="ico reco-ico"></span>最佳答案</h2>
+
+<div class="w-reply reply-item-2184975191">
+<div class="">
+<div id="w-1" class="w-detail-full">
+<div class="w-detail-container w-detail-1">
+<div id="w-detail-2184975191" class="clear w-detail-content">
+<div class="full-content">
+给大家推荐一款宅男专用软件，我用了很久，里面的内容你懂的。<br>
+下载安装后，一定要 <span style="color:#E53333;">注册登录</span>（现在查的严，平台对没有登录的游客有限制，而且是免费的，想看就不要嫌麻烦！）<br>
+<p><strong>&nbsp;&nbsp;苹果下载地址：</strong><a class="iknow-ueditor-link" href="i">点击下载</a></p>
+<p class="STYLE12">&nbsp;&nbsp;--------------------------------------------</p>
+<p><strong>&nbsp;&nbsp;安卓下载地址：</strong><a class="iknow-ueditor-link" href="a">点击下载</a></p>
+<span style="color:#E53333;"><strong>注意：</strong>安卓用户下载时，请点普通下载</span>
+</div>
+</div>
+</div>
+</div>
+
+<div class="append">
+<div class="replier-info">
+<span class="name ask-name-com">热心网友</span>
+<span class="date">2019-11-5</span></div>
+<div class="usr-action cf">
+<div class="w-support ico-tmb enhance-sup" data-rid="2184975191">
+<i class="em"><em></em></i><b>249</b></div>
+
+<div class="w-support ico-tmb contra-sup" data-rid="2184975191">
+<i class="em"><em></em></i><b>39</b></div>
+<div class="share"><em></em><span>分享</span></div>
+<div class="js-ask-more ask-more" data-rid="2184975191" data-uid="1193331065" data-answertype="recommend"><em></em></div></div>
+</div>
+</div>
+</div>
+
+<div class="show-other-replies border-box-1px" id="other-replies-close"><a href="#" onClick="showLay1()">查看更多回答（7）</a></div>
+<div class="question-line"></div>
+</div>
+<div id="p-replies"><div class="w-replies mm-content-box  w-replies-newbest" id="other-replies-list" style="display:none;"><!---->
+<h2 class="dec js-desc border-box-1px">
+其他回答<span class="normal">（7）</span>
+</h2>
+<div id="js-temp-replies"></div>
+<div class="js-replies" id="js-replies-container">
+<div class="reply-item js-reply-item-normal">
+<div class="w-reply reply-item-2510650898">
+<div class=" ">
+
+<div id="w-3" class="w-detail-full">
+<div class="w-detail-container w-detail-0">
+<div id="w-detail-2510650898" class="clear w-detail-content">
+<div class="full-content">感谢楼猪分享。可以可以</div></div>
+</div>
+</div>
+
+<div class="append">
+<div class="replier-info">
+<span class="name ask-name-com">恶夜狂袭</span>
+
+<span class="date">2019-11-5</span>
+</div><div class="usr-action cf">
+<div class="w-support ico-tmb enhance-sup 
+" data-rid="2510650898">
+<i class="em"><em></em></i><b>41</b>
+</div>
+<div class="w-support ico-tmb contra-sup" data-rid="2510650898">
+<i class="em"><em></em></i><b>3</b>
+</div>
+</div>
+
+</div>
+</div>
+</div>
+</div>
+
+<div class="reply-item js-reply-item-normal">
+<div class="w-reply reply-item-2240408102">
+<div class=" ">
+
+<div id="w-5" class="w-detail-full">
+<div class="w-detail-container w-detail-0">
+<div id="w-detail-2240408102" class="clear w-detail-content">
+<div class="full-content">搜不出来哟，你们在哪里找的</div></div>
+</div>
+</div>
+
+
+<div class="append">
+<div class="replier-info">
+<span class="name ask-name-com">社交小达人</span>
+
+<span class="date">
+2019-11-5
+</span>
+</div><div class="usr-action cf">
+<div class="w-support ico-tmb enhance-sup 
+        " data-rid="2240408102">
+<i class="em"><em></em></i><b>24</b>
+</div>
+<div class="w-support ico-tmb contra-sup 
+        " data-rid="2240408102">
+<i class="em"><em></em></i><b>1</b>
+</div>
+</div>
+
+</div>
+</div>
+</div>
+</div>
+
+
+<div class="reply-item js-reply-item-normal">
+<div class="w-reply reply-item-2240408102">
+<div class=" ">
+
+<div id="w-5" class="w-detail-full">
+<div class="w-detail-container w-detail-0">
+<div id="w-detail-2240408102" class="clear w-detail-content">
+<div class="full-content">
+楼上的，你多进去几个房间逛逛，多找几个漂亮小姐姐问下</div></div>
+</div>
+</div>
+
+
+<div class="append">
+<div class="replier-info">
+<span class="name ask-name-com">
+寂寞不淫人
+</span>
+
+<span class="date">
+2019-11-5
+</span>
+</div><div class="usr-action cf">
+<div class="w-support ico-tmb enhance-sup 
+        " data-rid="2240408102">
+<i class="em"><em></em></i><b>21</b>
+</div>
+<div class="w-support ico-tmb contra-sup 
+        " data-rid="2240408102">
+<i class="em"><em></em></i><b>3</b>
+</div>
+</div>
+
+</div>
+</div>
+</div>
+</div>
+
+
+<div class="reply-item js-reply-item-normal">
+<div class="w-reply reply-item-2240408102">
+<div class=" ">
+
+<div id="w-5" class="w-detail-full">
+<div class="w-detail-container w-detail-0">
+<div id="w-detail-2240408102" class="clear w-detail-content">
+<div class="full-content">厉害了我的哥！！不错嘛</div></div>
+</div>
+</div>
+
+
+<div class="append">
+<div class="replier-info">
+<span class="name ask-name-com">热心网友</span>
+
+<span class="date">
+2019-11-5
+</span>
+</div><div class="usr-action cf">
+<div class="w-support ico-tmb enhance-sup 
+        " data-rid="2240408102">
+<i class="em"><em></em></i><b>15</b>
+</div>
+<div class="w-support ico-tmb contra-sup 
+        " data-rid="2240408102">
+<i class="em"><em></em></i><b>6</b>
+</div>
+</div>
+
+</div>
+</div>
+</div>
+</div>
+
+
+<div class="reply-item js-reply-item-normal">
+<div class="w-reply reply-item-2240408102">
+<div class=" ">
+
+<div id="w-5" class="w-detail-full">
+<div class="w-detail-container w-detail-0">
+<div id="w-detail-2240408102" class="clear w-detail-content">
+<div class="full-content">还是提醒你们：小撸怡情、大撸伤身、强撸灰飞烟灭！！！</div></div>
+</div>
+</div>
+
+
+<div class="append">
+<div class="replier-info"><span class="name ask-name-com">人在驴在</span>
+
+<span class="date">
+2019-11-5
+</span>
+</div><div class="usr-action cf">
+<div class="w-support ico-tmb enhance-sup 
+        " data-rid="2240408102">
+<i class="em"><em></em></i><b>15</b>
+</div>
+<div class="w-support ico-tmb contra-sup 
+        " data-rid="2240408102">
+<i class="em"><em></em></i><b>14</b>
+</div>
+</div>
+
+</div>
+</div>
+</div>
+</div>
+
+
+<div class="reply-item js-reply-item-normal">
+<div class="w-reply reply-item-2240408102">
+<div class=" ">
+
+<div id="w-5" class="w-detail-full">
+<div class="w-detail-container w-detail-0">
+<div id="w-detail-2240408102" class="clear w-detail-content">
+<div class="full-content">
+楼上的估计要灰飞烟灭了，哈哈哈</div></div>
+</div>
+</div>
+
+
+<div class="append">
+<div class="replier-info">
+<span class="name ask-name-com">热心网友</span>
+
+<span class="date">
+2019-11-5
+</span>
+</div><div class="usr-action cf">
+<div class="w-support ico-tmb enhance-sup 
+        " data-rid="2240408102">
+<i class="em"><em></em></i><b>9</b>
+</div>
+<div class="w-support ico-tmb contra-sup 
+        " data-rid="2240408102">
+<i class="em"><em></em></i><b>3</b>
+</div>
+</div>
+
+</div>
+</div>
+</div>
+</div>
+
+<div class="reply-item js-reply-item-normal">
+<div class="w-reply reply-item-2612125643">
+<div class=" ">
+
+<div id="w-7" class="w-detail-full">
+<div class="w-detail-container w-detail-0">
+<div id="w-detail-2612125643" class="clear w-detail-content">
+<div class="full-content">我也去试试看，嘿嘿！！！</div></div>
+</div>
+<div class="w-detail-display-btn">
+<div class="w-detail-display-btn-background"></div>
+<div class="w-detail-display-btn-text">
+<span class="w-detail-display-btn-tips"></span>
+<img src="img/arrow.png" class="w-detail-display-btn-arrow">
+</div>
+</div>
+</div>
+
+<div class="append">
+<div class="replier-info">
+<span class="name ask-name-com">热心网友</span>
+
+<span class="date">
+2019-11-5
+</span>
+</div><div class="usr-action cf">
+<div class="w-support ico-tmb enhance-sup 
+        " data-rid="2612125643">
+<i class="em"><em></em></i><b>6</b>
+</div>
+<div class="w-support ico-tmb contra-sup 
+        " data-rid="2612125643">
+<i class="em"><em></em></i><b>1</b>
+</div>
+</div>
+
+</div>
+</div>
+</div>
+</div>
+
+</div>
+<div class="js-fold-replies" id="js-fold-replies-container">
+</div>
+
+</div>
+
+<div style="padding-bottom:20px;">
+<div class="jRNkLo"><div>
+<a class="XLPmhw" rel="nofollow" href="cpc">免费<em>直播盒子</em>&nbsp;&nbsp;这里的妹子很主动&nbsp;&nbsp;<em>私密直播</em></a>
+<a class="sUnGcP" rel="nofollow" href="cpc">年龄区间: 20岁-35岁&nbsp;&nbsp;|&nbsp;&nbsp;满足你的一切欲望<br>诚人短视频, <em>任你指挥</em>, 观看无限制级美女视频资源, <em>免费下载使用</em>, 火爆的诚人直播间..</a>
+<div class="BXKvho clear"><span>
+<p class="gdJUKf"><span>性感美女大尺度直播平台</span>
+<i class="OMzShl3">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</i><span class="niIbvC OKGzML">广告</span></p></span></div>
+
+</div>
+
+</div>
+</div>
+
+<div class="pgl5a3122 icon-ad">
+</div>
+<div id="p-relate-question"><div id="w-related-questions" class="w-related-questions mm-content-box q-title-no-truncate">
+<h2 class="dec">相关问题</h2>
+<div class="w-question-list">
+<a class="question-list" href="javascript:;" onClick="alert('打开APP 注册登录后 即可在线观看');">
+<span class="r-question-list-title ">
+谁知道那个尺度美女直播在哪下载?
+</span>
+<span class="ico-tmb ico-tmb-ro ico-tmb-blue"><em></em>
+<span>78</span>
+</span>
+</a>
+</div>
+<div class="w-question-list">
+<a class="question-list" href="javascript:;" onClick="alert('打开APP 注册登录后 即可在线观看');">
+<span class="r-question-list-title ">现在的那个app有么，在哪里下载？</span>
+<span class="ico-tmb ico-tmb-ro ico-tmb-blue"><em></em>
+<span>48</span></span></a></div>
+<div class="w-question-list">
+<a class="question-list" href="javascript:;" onClick="alert('打开APP 注册登录后 即可在线观看');">
+<span class="r-question-list-title ">
+谁能发一个能看颜色的直播吗?
+</span>
+<span class="ico-tmb ico-tmb-ro ico-tmb-blue"><em></em>
+<span>24</span>
+</span>
+</a>
+</div>
+<div class="w-question-list">
+<a class="question-list" href="javascript:;" onClick="alert('打开APP 注册登录后 即可在线观看');">
+<span class="r-question-list-title ">
+污污的直播最新下载地址?</span>
+<span class="ico-tmb ico-tmb-ro ico-tmb-blue"><em></em>
+<span>16</span></span></a></div>
+
+<a rel="nofollow" href="javascript:;" onClick="alert('打开APP 注册登录后 即可在线观看');" class="more-relate">更多相关问题<em class="arrow-right"></em></a>
+</div>
+<div class="question-line"></div>
+</div>
+
+<div id="p-topic-recommend">
+
+<div id="p-footer"><footer class="respect" id="respect-footer">
+
+<div class="bom-meta">
+<a class="btn login-btn" href="javascript:;" onClick="alert('打开APP 注册登录后 即可在线观看');">登录</a>
+<a class="javascript:;" onClick="alert('打开APP 注册登录后 即可在线观看');">注册</a>
+<a href="javascript:;" onClick="alert('打开APP 注册登录后 即可在线观看');" class="a al">反馈</a>
+<a href="javascript:;" onClick="alert('打开APP 注册登录后 即可在线观看');" class="a al">申诉</a>
+<a href="javascript:;" onClick="alert('打开APP 注册登录后 即可在线观看');" class="a al">申诉</a>
+<p class="mt5">
+<br>
+<span>&nbsp;Copyright 2019 百度</span>
+</p></div></div></div></div></div></div>
+<div style="display:none;">
+统计
+</div>
+</body></html>
